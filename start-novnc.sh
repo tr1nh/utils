3abfb9414ec0172ec4d78703162e8d6552e91684
Khánh Trình <trandiepkhanhtrinh@gmail.com>
2024-11-26 09:59:46 +0700

    fix: assign localtion for .passwd

--- start-novnc.sh
+++ start-novnc.sh
@@ -3,20 +3,21 @@
 # create a new VNC session with i3 window manager
 # and noVNC web client
 
+_NOVNC_PASSWD="$HOME/.passwd"
 _NOVNC_PATH="$HOME/noVNC"
 _DISPLAY=:1
 _GEOMETRY=800x600
 _PORT=8901
 
-if [ -f ~/.vnc/passwd ]; then
+if [ -f "$_NOVNC_PASSWD" ]; then
     echo "VNC password already set"
 else
   echo "Input VNC password:"
-  vncpasswd
+  vncpasswd "$_NOVNC_PASSWD"
 fi
 
-${_NOVNC_PATH}/utils/novnc_proxy --listen ${_PORT} --vnc localhost:5901 &
-Xvnc ${_DISPLAY} -geometry ${_GEOMETRY} -rfbauth ~/.vnc/passwd &
+${_NOVNC_PATH}/utils/novnc_proxy --listen ${_PORT} --vnc 127.0.0.1:5901 &
+Xvnc ${_DISPLAY} -geometry ${_GEOMETRY} -rfbauth "$_NOVNC_PASSWD" &
 
 sleep 2
 DISPLAY=${_DISPLAY} i3
