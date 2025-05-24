7602cb790b1fb39b7ddb166046fe4ad1a78b252c
Khánh Trình <trandiepkhanhtrinh@gmail.com>
2025-05-24 19:48:29 +0700

    feat: added config vnc port

--- start-novnc.sh
+++ start-novnc.sh
@@ -8,6 +8,7 @@ _NOVNC_PATH="$HOME/noVNC"
 _DISPLAY=:1
 _GEOMETRY=800x600
 _PORT=8901
+_VNC_PORT=5901
 
 if [ -f "$_NOVNC_PASSWD" ]; then
     echo "VNC password already set"
@@ -16,8 +17,8 @@ else
   vncpasswd "$_NOVNC_PASSWD"
 fi
 
-${_NOVNC_PATH}/utils/novnc_proxy --listen ${_PORT} --vnc 127.0.0.1:5901 &
-Xvnc ${_DISPLAY} -geometry ${_GEOMETRY} -rfbauth "$_NOVNC_PASSWD" &
+${_NOVNC_PATH}/utils/novnc_proxy --listen ${_PORT} --vnc "127.0.0.1:$_VNC_PORT" &
+Xvnc ${_DISPLAY} -geometry ${_GEOMETRY} -rfbauth "$_NOVNC_PASSWD" -rfbport ${_VNC_PORT} &
 
 sleep 2
 DISPLAY=${_DISPLAY} i3
