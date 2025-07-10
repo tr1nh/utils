f3703133cd1effe60c8526fd0475fddf92f9e2ad
Khánh Trình <trandiepkhanhtrinh@gmail.com>
2025-07-10 14:14:32 +0700

    feat: check noVNC folder exists and change default port

--- start-novnc.sh
+++ start-novnc.sh
@@ -6,15 +6,20 @@
 _NOVNC_PASSWD="$HOME/.passwd"
 _NOVNC_PATH="$HOME/noVNC"
 _DISPLAY=:1
-_GEOMETRY=800x600
-_PORT=8901
+_GEOMETRY=800x800
+_PORT=8080
 _VNC_PORT=5901
 
 if [ -f "$_NOVNC_PASSWD" ]; then
-    echo "VNC password already set"
+  echo "VNC password already set"
 else
   echo "Input VNC password:"
   vncpasswd "$_NOVNC_PASSWD"
+fi
+
+if [[ ! -d "$HOME/noVNC" ]]; then
+  git clone --depth=1 https://github.com/novnc/noVNC "$HOME/noVNC"
+  curl -L https://raw.githubusercontent.com/tr1nh/utils/master/vnc_mobile.html -o "$HOME/noVNC/vnc_mobile.html"
 fi
 
 ${_NOVNC_PATH}/utils/novnc_proxy --listen ${_PORT} --vnc "127.0.0.1:$_VNC_PORT" &
