0546e172a5627ede43153863219139433dfebb55
Khánh Trình <trandiepkhanhtrinh@gmail.com>
2026-09-01 19:41:44 +0700

    feat: updated install-kali-package.sh

--- install-kali-package.sh
+++ install-kali-package.sh
@@ -2,13 +2,12 @@
 
 apt update
 
-apt install bash bash-completion sudo screen man-db busybox \
-    xsel xclip xdotool xserver-xorg-core \
+apt install bash bash-completion sudo screen man-db busybox locales \
+    xsel xclip xdotool xserver-xorg-core dbus-x11 wmctrl \
     i3-wm libnotify-bin dunst dmenu fonts-noto-core fonts-noto-cjk fonts-noto-extra \
     tigervnc-standalone-server tigervnc-common tigervnc-xorg-extension remmina remmina-plugin-vnc remmina-plugin-rdp \
     rxvt-unicode \
-    p7zip-full unrar \
-    openssl openssh-server rsync sshfs wget curl netcat-openbsd firefox-esr \
+    p7zip-full unrar-free \
+    openssl openssh-server rsync sshfs wget curl netcat-openbsd proxychains tor \
     maim imagemagick feh ffmpeg \
-    vim jq git build-essential python3 python3-pip pipx -y
-    
+    vim jq git build-essential python3 -y
